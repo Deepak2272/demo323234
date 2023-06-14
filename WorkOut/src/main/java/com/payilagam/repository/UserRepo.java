d4a5e030c90789ae356a7d4598ea5254c11da6c3
unknown <deepak2272000@gmail.com>
2023-06-14 22:11:41 +0530

    added public

--- WorkOut/src/main/java/com/payilagam/repository/UserRepo.java
+++ WorkOut/src/main/java/com/payilagam/repository/UserRepo.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import com.payilagam.model.User;
 
 public interface UserRepo extends CrudRepository<User,Integer> {
-    findbyname(String name);
+    public findbyname(String name);
 }
