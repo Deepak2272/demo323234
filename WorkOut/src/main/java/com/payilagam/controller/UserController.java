81b90c6db8977f31fc38cb02bcb69c5a14c53a50
unknown <deepak2272000@gmail.com>
2023-09-30 13:07:09 +0530

    added comments in controller

--- WorkOut/src/main/java/com/payilagam/controller/UserController.java
+++ WorkOut/src/main/java/com/payilagam/controller/UserController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.payilagam.repository.UserRepo;
-
+// hi
 @RestController
 public class UserController {
 
